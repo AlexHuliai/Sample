231b47a6dd80a84b0ecc16505b96bfc1c3d9634a
Oleksandr Huliai <58236141+AlexHuliai@users.noreply.github.com>
2020-04-29 18:11:38 -0500

    Initial commit

---
+++ src/com/huliai/Test.java
@@ -0,0 +1,16 @@
+package com.huliai;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+
+    }
+
+}
