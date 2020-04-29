6c3f50c52d76e8e08b32c6fe5f665a9739940b40
Oleksandr Huliai <58236141+AlexHuliai@users.noreply.github.com>
2020-04-29 18:12:23 -0500

    Java is back

--- src/com/huliai/Test.java
+++ src/com/huliai/Test.java
@@ -5,12 +5,7 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-
-
+        
     }
 
 }
